0789a8a706a5e8fcaf6a217cf79ac182a7864aa1
Pedro Palencia <pedropalencia7@gmail.com>
2021-10-29 01:41:24 -0300

    styles and media queries modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,20 +10,20 @@ function Header() {
             <div className="logo">CODER<span>SUMMIT21</span></div>
                         
             <div className="mainContainer">
-                <div className="main__date">&lt; 21_<span>NOV</span>_2021 &gt;</div>
+                <div className="main__date">&lt; 21_<span>NOV_</span>2021 &gt;</div>
                 <div className="main_title_container">
-                    <div className="main__title">SE PARTE DE LO QUE SE VIENE</div>
+                    <div className="main__title">SÉ PARTE DE LO QUE SE VIENE</div>
                 </div>
             </div>
             
             <SingUpBox />
 
             <div className="socialMedia">
-                <div><BsInstagram className="socialMedia_icon"/></div>
-                <div><BsYoutube className="socialMedia_icon"/></div>
-                <div><BsLinkedin className="socialMedia_icon"/></div>
-                <div><BsTwitter className="socialMedia_icon"/></div>
-                <div><BsFacebook className="socialMedia_icon"/></div>
+                <a href="#"> <BsInstagram className="socialMedia_icon"/> </a>
+                <a href="#"> <BsYoutube className="socialMedia_icon"/> </a>
+                <a href="#"> <BsLinkedin className="socialMedia_icon"/> </a>
+                <a href="#"> <BsTwitter className="socialMedia_icon"/> </a>
+                <a href="#"> <BsFacebook className="socialMedia_icon"/> </a>
             </div>
 
         </div>
