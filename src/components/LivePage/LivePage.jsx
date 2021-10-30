add9a350a393b3ff4ef0f8b27178941773ba180e
Pedro Palencia <pedropalencia7@gmail.com>
2021-10-30 19:21:20 -0300

    social medida in footer added

--- src/components/LivePage/LivePage.jsx
+++ src/components/LivePage/LivePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsLinkedin, BsInstagram, BsYoutube, BsTwitter, BsFacebook } from 'react-icons/bs';
 import './LivePage.css'
 import 'animate.css';
 
@@ -23,8 +24,19 @@ function LivePage() {
 
             <div className="livePage_footer_container">
                 <div className="livepage_footer">
+                    
                     <div className="livepage_footer__line"></div>
-                    <div className="logo_livePage logo--footer animate__animated animate__fadeInDown">CODER<span>SUMMIT21</span></div>
+                    
+                    <div className="logo_livePage--footer animate__animated animate__fadeInDown">CODER<span>SUMMIT21</span></div>
+
+                    <div className="socialMedia--footer">
+                        <a href="https://www.instagram.com/coderhouse/" target="_blank"> <BsInstagram className="socialMedia--footer--icon"/> </a>
+                        <a href="https://www.youtube.com/channel/UCeFRG6700bzqYI0M_9vyelw/videos" target="_blank"> <BsYoutube className="socialMedia--footer--icon"/> </a>
+                        <a href="https://www.linkedin.com/school/coderhouse/" target="_blank"> <BsLinkedin className="socialMedia--footer--icon"/> </a>
+                        <a href="https://twitter.com/CODERHOUSE" target="_blank"> <BsTwitter className="socialMedia--footer--icon"/> </a>
+                        <a href="https://www.facebook.com/Coderhouse/" target="_blank"> <BsFacebook className="socialMedia--footer--icon"/> </a>
+                    </div>
+
                 </div>
             </div>
         </div>
